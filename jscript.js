ce224e06eb31c190b17afb448d092c495acf1698
Kgoomba <Jdraggoo73@gmail.com>
2022-09-27 14:47:45 -0400

    Added a button for users to change the size of the canvas.

--- jscript.js
+++ jscript.js
@@ -6,19 +6,36 @@ const scaleBtn = document.querySelector("#scaleBtn");
 
 scaleBtn.classList.add("scaleBtn");
 
-/*for(i=0; i<256; i++){
+for(i=0; i<256; i++){
     var tile = document.createElement("div");
     tile.className = "blah";
+    tile.addEventListener("mouseover", (e) =>  e.target.style.backgroundColor = "red");
     container.appendChild(tile);
-}*/
+}
 
-container.addEventListener("mouseover", (e) =>  e.target.style.backgroundColor = "red");
+var gridSize = 256
 
 scaleBtn.addEventListener("click", (e) => {
-    let scale = prompt("How many squares per side of the new grid?")});
-
-
-
-
-
+    /*A do...while loop was used to repeat the prompt to the user if they input
+    something that didn't meet the requirements. This works, but causes the cancel
+    button to repeat the prompt instead of exiting. This should be revised later.*/
+    do { 
+        var scale = prompt("Enter the number of tiles per side. Please enter a number between 16-100.")
+    } while (scale < 16 || scale > 100 || isNaN(scale) == true)
+        if (scale > 15 && scale <101 && isNaN(scale) == false){
+            let oldTiles = document.querySelectorAll(".blah")
+            oldTiles.forEach(tile => {
+                tile.remove()
+            });
+        gridSize = scale * scale;
+        container.setAttribute("style", `grid-template-columns: repeat(${scale}, 1fr); 
+            grid-template-rows: repeat(${scale}, 1fr);`);
+        for(i=0; i<gridSize; i++){
+            var tile = document.createElement("div");
+            tile.className = "blah";
+            tile.addEventListener("mouseover", (e) =>  e.target.style.backgroundColor = "red");
+            container.appendChild(tile);
+      }
+    } else {}
+});
 
