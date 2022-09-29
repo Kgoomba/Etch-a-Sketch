bd8bbc160e9db5615923c350e7ff8a7510c31f57
Kgoomba <Jdraggoo73@gmail.com>
2022-09-29 18:02:37 -0400

    comment

--- jscript.js
+++ jscript.js
@@ -1,15 +1,18 @@
 const container = document.querySelector("#container");
+const scaleBtn = document.querySelector("#scaleBtn");
+const rainbowBtn = document.querySelector("#rainbowBtn");
+const gradientBtn = document.querySelector("#gradientBtn");
+const clearBtn = document.querySelector("#clearBtn");
 
 container.classList.add("grid-con");
-
-const scaleBtn = document.querySelector("#scaleBtn");
-
-scaleBtn.classList.add("scaleBtn");
+scaleBtn.classList.add("Btn");
+rainbowBtn.classList.add("Btn")
+gradientBtn.classList.add("Btn")
+clearBtn.classList.add("Btn")
 
 for(i=0; i<256; i++){
     var tile = document.createElement("div");
     tile.className = "blah";
-    tile.addEventListener("mouseover", (e) =>  e.target.style.backgroundColor = "red");
     container.appendChild(tile);
 }
 
@@ -33,9 +36,41 @@ scaleBtn.addEventListener("click", (e) => {
         for(i=0; i<gridSize; i++){
             var tile = document.createElement("div");
             tile.className = "blah";
-            tile.addEventListener("mouseover", (e) =>  e.target.style.backgroundColor = "red");
             container.appendChild(tile);
       }
     } else {}
 });
 
+var btnValue = "black"
+
+rainbowBtn.addEventListener("click", (e) => {
+    btnValue = "rainbow"
+    colorChg()
+})
+
+clearBtn.addEventListener("click", (e) => {
+    let oldTiles = document.querySelectorAll(".blah")
+        oldTiles.forEach(tile => {
+        tile.style.backgroundColor = "white"})
+            btnValue = "black"
+            colorChg()
+})
+
+function randCol(){
+    randRGB = Math.floor(Math.random() * 256)
+    return randRGB
+}
+
+colorChg(btnValue)
+
+function colorChg(){
+    if (btnValue == "rainbow"){
+        container.addEventListener("mouseover", (e) => 
+        e.target.style.backgroundColor = `rgb(${randCol()}, ${randCol()}, ${randCol()}`)
+    } else if (btnValue == "black"){
+        container.addEventListener("mouseover", (e) =>
+        e.target.style.backgroundColor = "black")
+    }
+/*In here will be the opacity increase clause for the gradientBtn when I've cleared my head enough
+to come back and figure it out.*/
+}
